fb5462221af65649194be30f90ed89a9a1fb2c2d
oGraebin <felipegraebin@hotmail.com>
2021-11-10 15:40:34 -0300

    Implementação Get-Post Tema

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://ograebin.herokuapp.com/tema', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://ograebin.herokuapp.com/tema', tema, this.token)
+  }
+
+}
